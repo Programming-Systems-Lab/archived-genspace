f5cda9e426090a741780ac33e015d2b19f77f6a0
Flavio Palandri Antonelli <fp2229@columbia.edu>
2010-12-26 23:01:41 -0500

    test

--- genspace-client/client/src/org/geworkbench/components/genspace/RuntimeEnvironmentSettings.java
+++ genspace-client/client/src/org/geworkbench/components/genspace/RuntimeEnvironmentSettings.java
@@ -5,22 +5,31 @@ public class RuntimeEnvironmentSettings {
 	public static final String YORK = "york.cs.columbia.edu";
 	public static final String BORIS = "boris.cs.columbia.edu";
 	public static final String BAMBI = "bambi.cs.columbia.edu";
-	
-	public static final String PROD_HOST = BAMBI;
-	public static final String DEVEL_HOST = BORIS;
-
-	public static final ServerConfig EVENT_SERVER = new ServerConfig(PROD_HOST, 12346);
-	public static final ServerConfig WORKFLOW_SERVER = new ServerConfig(PROD_HOST, 22222);
-	public static final ServerConfig NETWORK_VIS_SERVER = new ServerConfig(DEVEL_HOST, 8282);
-	public static final ServerConfig LOOKUP_SERVER = new ServerConfig(PROD_HOST, 12346);
-	public static final ServerConfig ISBU_SERVER = new ServerConfig(BORIS, 12345);
-	public static final ServerConfig TOOL_SERVER = new ServerConfig(BORIS, 12346);
-	
+	public static final String LOCAL = "localhost";
 	
 	public static final String LOOKUP_HOST = "york.cs.columbia.edu";
 	public static final int LOOKUP_PORT = 44444;
+	
+	public static final String PROD_HOST = BAMBI;
+	public static final String DEVEL_HOST = BORIS;
+	public static final String LOCAL_HOST = LOCAL;
+	
+	public static final String SERVER = LOCAL_HOST;
+
+	public static final ServerConfig EVENT_SERVER = new ServerConfig(SERVER, 12346);
+	public static final ServerConfig WORKFLOW_SERVER = new ServerConfig(SERVER, 12343);
+	public static final ServerConfig WORKFLOW_VIS_SERVER = new ServerConfig(SERVER, 12343);
+	public static final ServerConfig NETWORK_VIS_SERVER = new ServerConfig(SERVER, 12344);
+	public static final ServerConfig LOOKUP_SERVER = new ServerConfig(LOOKUP_HOST, 12346);
+	public static final ServerConfig ISBU_SERVER = new ServerConfig(SERVER, 12345);
+	public static final ServerConfig TOOL_SERVER = new ServerConfig(SERVER, 12341);
+	public static final ServerConfig SECURITY_SERVER = new ServerConfig(SERVER, 12347);
 
 	public static final String DEFAULT_USER = "";
 	
-	public static final String GS_WEB_ROOT = "http://lenox.cs.columbia.edu/genspace/";
+	public static final String GS_WEB_ROOT_PROD = "http://bambi.cs.columbia.edu/";
+	public static final String GS_WEB_ROOT_DEVEL = "http://lenox.cs.columbia.edu/genspace/";
+	
+	public static final String GS_WEB_ROOT = GS_WEB_ROOT_PROD;
+	
 }
