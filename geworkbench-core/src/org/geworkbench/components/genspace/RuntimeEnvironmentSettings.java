696f2fc90b95407b9d4cf9dbf71724e3887bcee8
Jonathan Bell <jbell@cs.columbia.edu>
2011-04-18 01:47:36 -0400

    Updated environemtn settings to use prod website

--- geworkbench-core/src/org/geworkbench/components/genspace/RuntimeEnvironmentSettings.java
+++ geworkbench-core/src/org/geworkbench/components/genspace/RuntimeEnvironmentSettings.java
@@ -14,10 +14,10 @@ public class RuntimeEnvironmentSettings {
 	public static final String PROD_HOST = "genspace.cs.columbia.edu";
 	public static final String DEVEL_HOST = "amos.cs.columbia.edu";
 	public static final String LOCAL_HOST = "localhost";
-	public static final String SERVER = DEVEL_HOST;
-	public static final String XMPP_HOST = DEVEL_HOST;
+	public static final String SERVER = PROD_HOST;
+	public static final String XMPP_HOST = PROD_HOST;
 
-	public static final String GS_WEB_ROOT_PROD = "http://bambi.cs.columbia.edu/";
+	public static final String GS_WEB_ROOT_PROD = "http://genspace.cs.columbia.edu/";
 	public static final String GS_WEB_ROOT_DEVEL = "http://lenox.cs.columbia.edu/genspace/";
 
 	public static final String GS_WEB_ROOT = GS_WEB_ROOT_PROD;
