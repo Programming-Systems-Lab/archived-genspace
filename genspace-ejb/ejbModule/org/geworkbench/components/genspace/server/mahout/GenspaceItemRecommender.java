977d2097f98929ced70485a06299f0d6dd17d4eb
Evgeny Fedotov <ef2363@columbia.edu>
2011-07-30 23:50:11 -0400

    added mahout classes

---
+++ genspace-ejb/ejbModule/org/geworkbench/components/genspace/server/mahout/GenspaceItemRecommender.java
@@ -0,0 +1,102 @@
+package org.geworkbench.components.genspace.server.mahout;
+
+import java.util.List;
+
+import org.apache.mahout.cf.taste.recommender.CandidateItemsStrategy;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import org.apache.mahout.cf.taste.common.Refreshable;
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.common.FastIDSet;
+import org.apache.mahout.cf.taste.impl.common.FullRunningAverage;
+import org.apache.mahout.cf.taste.impl.common.RefreshHelper;
+import org.apache.mahout.cf.taste.impl.common.RunningAverage;
+import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
+import org.apache.mahout.cf.taste.impl.recommender.TopItems;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.model.PreferenceArray;
+import org.apache.mahout.cf.taste.recommender.IDRescorer;
+import org.apache.mahout.cf.taste.recommender.ItemBasedRecommender;
+import org.apache.mahout.cf.taste.recommender.MostSimilarItemsCandidateItemsStrategy;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Rescorer;
+import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
+import org.apache.mahout.common.LongPair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Preconditions;
+
+public class GenspaceItemRecommender extends GenericItemBasedRecommender{
+
+	private ItemSimilarity similarity;
+	
+	public GenspaceItemRecommender(DataModel dataModel,
+			ItemSimilarity similarity) {
+		super(dataModel, similarity);
+		this.similarity = similarity;
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	  public List<RecommendedItem> recommend(long userID, int howMany, IDRescorer rescorer) throws TasteException {
+	    Preconditions.checkArgument(howMany >= 1, "howMany must be at least 1");
+	    PreferenceArray preferencesFromUser = getDataModel().getPreferencesFromUser(userID);
+	    if (preferencesFromUser.length() == 0) {
+	      return Collections.emptyList();
+	    }
+	    
+	   
+
+	    FastIDSet possibleItemIDs = getAllOtherItems(userID, preferencesFromUser);
+
+	    TopItems.Estimator<Long> estimator = new MultiMostSimilarEstimator(preferencesFromUser.getIDs(), similarity, null, true);
+
+	    List<RecommendedItem> topItems = TopItems.getTopItems(howMany, possibleItemIDs.iterator(), rescorer,
+	      estimator);
+
+	    return topItems;
+	  }
+	
+	private static final class MultiMostSimilarEstimator implements TopItems.Estimator<Long> {
+	    
+	    private final long[] toItemIDs;
+	    private final ItemSimilarity similarity;
+	    private final Rescorer<LongPair> rescorer;
+	    private final boolean excludeItemIfNotSimilarToAll;
+	    
+	    private MultiMostSimilarEstimator(long[] toItemIDs, ItemSimilarity similarity, Rescorer<LongPair> rescorer,
+	        boolean excludeItemIfNotSimilarToAll) {
+	      this.toItemIDs = toItemIDs;
+	      this.similarity = similarity;
+	      this.rescorer = rescorer;
+	      this.excludeItemIfNotSimilarToAll = excludeItemIfNotSimilarToAll;
+	    }
+	    
+	    @Override
+	    public double estimate(Long itemID) throws TasteException {
+	      RunningAverage average = new FullRunningAverage();
+	      double[] similarities = similarity.itemSimilarities(itemID, toItemIDs);
+	      for (int i = 0; i < toItemIDs.length; i++) {
+	        long toItemID = toItemIDs[i];
+	        LongPair pair = new LongPair(toItemID, itemID);
+	        if (rescorer != null && rescorer.isFiltered(pair)) {
+	          continue;
+	        }
+	        double estimate = similarities[i];
+	        if (rescorer != null) {
+	          estimate = rescorer.rescore(pair, estimate);
+	        }
+	        if (excludeItemIfNotSimilarToAll || !Double.isNaN(estimate)) {
+	          average.addDatum(estimate);
+	        }
+	      }
+	      double averageEstimate = average.getAverage();
+	      return averageEstimate == 0 ? Double.NaN : averageEstimate;
+	    }
+	  }
+
+}
---
+++ genspace-ejb/ejbModule/org/geworkbench/components/genspace/server/mahout/GenspaceItemSimilarity.java
@@ -0,0 +1,75 @@
+package org.geworkbench.components.genspace.server.mahout;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+import org.apache.mahout.cf.taste.common.Refreshable;
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.similarity.AbstractItemSimilarity;
+import org.apache.mahout.cf.taste.impl.similarity.EuclideanDistanceSimilarity;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.impl.similarity.SpearmanCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
+
+public class GenspaceItemSimilarity extends AbstractItemSimilarity{
+
+	private int PARENT_DEPTH = 3;
+	private int filtering;
+	private DataModel model;
+	private ItemSimilarity similarity;
+	private HashMap<Long, Long> parentMap;
+	
+	public GenspaceItemSimilarity(DataModel model, int filtering, HashMap<Long, Long> parentMap) throws TasteException {
+		super(model);
+		similarity = new EuclideanDistanceSimilarity(model);
+		this.filtering = filtering;
+		this.model = model;
+		this.parentMap = parentMap;
+	}
+
+	@Override
+	public double[] itemSimilarities(long arg0, long[] arg1)
+			throws TasteException {
+		int length = arg1.length;
+	    double[] result = new double[length];
+	    for (int i = 0; i < length; i++) {
+	      result[i] = itemSimilarity(arg0, arg1[i]);
+	    }
+	    return result;
+	}
+
+	@Override
+	public double itemSimilarity(long arg0, long arg1) throws TasteException {
+		// TODO Auto-generated method stub
+		int r = 0;
+		long currentId1 = arg0;
+		long currentId2 = arg1;
+		//System.out.println(MahoutTest.getParentMap().get(currentId1) + " " + MahoutTest.getParentMap().get(currentId2));
+		for (int i=0; i< PARENT_DEPTH; i++) {
+			if (!parentMap.containsKey(currentId1) || 
+					!parentMap.containsKey(currentId2))
+				break;
+			currentId1 = parentMap.get(currentId1);
+			currentId2 = parentMap.get(currentId2);
+			if (currentId1 * currentId2 == 0)
+				break;
+			else if (currentId1 == currentId2) {
+				r = i;
+			} else if (currentId1 == arg1 || currentId2 == arg0) {
+				r = i;
+			}
+		}
+		if (r == 0)
+			r = PARENT_DEPTH;
+		// r is between PARENT_DEPTH and 0;
+		double metrics = (double)-2 * r/PARENT_DEPTH + 1; // [-1;1]
+		
+		/*
+		if (metrics > -1)
+			System.out.println(metrics + " " + arg0 + " " + arg1);
+		*/	
+		return metrics;
+		//return similarity.itemSimilarity(arg0, arg1) * 3/4 + metrics * 1/4;
+	}
+}
